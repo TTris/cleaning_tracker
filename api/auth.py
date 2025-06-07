6344b46c2b1e1658e4a36eec55f6a053e3cc7b44
TTris725 <mctsai725@gmail.com>
2025-06-07 21:27:23 +0800

    9th - minor update JWT

--- api/auth.py
+++ api/auth.py
@@ -2,24 +2,34 @@ import os
 import jwt
 from dotenv import load_dotenv
 from flask import request, jsonify
+from functools import wraps
 
 load_dotenv()
 
 def get_user_id():
      # 藉由headers的authorization攜帶的bearer, decode jwt, 取得存放在supabase的userid   
      try:
-          token = request.headers.get("Authorization").replace("Bearer ","")
-          payload = jwt.decode(token, os.getenv("SUPABASE_JWT_SECRET"), algorithms=["HS256"],audience="authenticated")
-          return payload.get("sub")
+          header = request.headers.get("Authorization")
+          if (not header) or (not header.startswith("Bearer ")):
+               return jsonify({"Error": "Unauthorized"}), 401
+          token = header.split("")[1]
+          try:
+               payload = jwt.decode(token, os.getenv("SUPABASE_JWT_SECRET"), algorithms=["HS256"],audience="authenticated")
+               return payload.get("sub")
+          except jwt.ExpiredSignatureError:
+               return jsonify({"Error": "Token expired"}), 401
+          except jwt.InvalidTokenError:
+               return jsonify({"Error": "Invalid token"}), 401
+          
      except Exception as e:
-          print("JWT解碼錯誤：",e)
+          print("Error",str(e))
           return None
 
 def require_auth(func):
-    def wrapper(*args, **kwargs):
+    @wraps(func)
+    def decorated(*args, **kwargs):
         user_id = get_user_id()
         if not user_id:
-             return jsonify({"error":"Unauthorized"}), 401
+             return jsonify({"Error": "Unauthorized"}), 401
         return func(user_id=user_id, *args, **kwargs)
-    wrapper.__name__ = func.__name__
-    return wrapper 
+    return decorated 
